d47c7a244ecb1126ca9af4d920740a12c5078aeb
Eduardo <eduardo202811@gmail.com>
2022-10-31 21:53:45 -0300

    Vinculando fotos a alunos na base de dados

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -14,7 +14,6 @@ class UserController {
 
   async index(req, res) {
     try {
-      console.log(req.params.id);
       const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
--- src/database/index.js
+++ src/database/index.js
@@ -2,11 +2,12 @@ import Sequelize from 'sequelize';
 import databaseConfig from '../config/database';
 import Aluno from '../models/Aluno';
 import User from '../models/User';
+import Foto from '../models/Foto';
 
 /** Cada modelo criado deve ser importado
  * e adicionado dentro do Array.
  */
-const models = [Aluno, User];
+const models = [Aluno, User, Foto];
 
 const connection = new Sequelize(databaseConfig);
 models.forEach((model) => model.init(connection));
